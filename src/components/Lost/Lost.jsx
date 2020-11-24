d48770a015b4315f3e830c021986b9ba580c9359
_uchanlee <uchanlee.dev@gmail.com>
2020-11-25 01:45:44 +0900

    ADD: connect container

--- src/components/Lost/Lost.jsx
+++ src/components/Lost/Lost.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { BiSearch } from 'react-icons/bi';
-import './Lost.scss';
+import moment from 'moment';
 
-const Lost = ({}) => {
+import './Lost.scss';
+import SERVER from '../../config/server';
+
+const LostStatus = {
+  KEEP: '보관',
+  LOSTED: '분실',
+};
+
+const LostStatusColor = {
+  KEEP: 'green',
+  LOSTED: 'red',
+};
+
+const Lost = ({ getLosts, clearLostStore }) => {
+  useEffect(() => {
+    getLosts();
+
+    return () => {
+      clearLostStore();
+    };
+  }, []);
+  const losts = [
+    {
+      content: 'fffff',
+      createdAt: '2020-11-23T20:26:07.684Z',
+      idx: 1,
+      imageUrl: 'files-92631105-d0d3-4824-bfd5-0b091d5be4f0-48552260.jpeg',
+      location: 'ffff',
+      lostStatus: 'KEEP',
+      title: 'ffff',
+      userIdx: 2,
+      userName: 'admin',
+      userStudentId: '1234',
+    },
+    {
+      content: 'fffff',
+      createdAt: '2020-11-23T20:26:07.684Z',
+      idx: 2,
+      imageUrl: 'files-92631105-d0d3-4824-bfd5-0b091d5be4f0-48552260.jpeg',
+      location: 'ffff',
+      lostStatus: 'KEEP',
+      title: 'ffff',
+      userIdx: 2,
+      userName: 'admin',
+      userStudentId: '1234',
+    },
+    {
+      content: 'fffff',
+      createdAt: '2020-11-23T20:26:07.684Z',
+      idx: 3,
+      imageUrl: 'files-92631105-d0d3-4824-bfd5-0b091d5be4f0-48552260.jpeg',
+      location: 'ffff',
+      lostStatus: 'KEEP',
+      title: 'ffff',
+      userIdx: 2,
+      userName: 'admin',
+      userStudentId: '1234',
+    },
+    {
+      content: 'fffff',
+      createdAt: '2020-11-23T20:26:07.684Z',
+      idx: 4,
+      imageUrl: 'files-92631105-d0d3-4824-bfd5-0b091d5be4f0-48552260.jpeg',
+      location: 'ffff',
+      lostStatus: 'LOSTED',
+      title: 'ffff',
+      userIdx: 2,
+      userName: 'admin',
+      userStudentId: '1234',
+    },
+  ];
+
   return (
     <>
       <div className='lost'>
@@ -14,95 +85,45 @@ const Lost = ({}) => {
           </div>
         </div>
         <div className='lost-list'>
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
+          {losts.map(({ idx, imageUrl, content, location, createdAt, lostStatus }) => (
+            <div className='lost-list-content' key={idx}>
+              <div className='lost-list-content-image'>
+                {!!imageUrl ? (
+                  <img
+                    className='lost-list-content-image-fit'
+                    src={`${SERVER}/public/${imageUrl}`}
+                  />
+                ) : (
+                  <h4 className='lost-list-content-image-non_text'>
+                    이미지가 존재하지 않는 게시글입니다.
+                  </h4>
+                )}
+              </div>
+              <div className='lost-list-content-title'>{content}</div>
+              <div className='lost-list-content-place'>{location}</div>
+              <div className='lost-list-content-area'>
+                <div className='lost-list-content-area-date'>
+                  {moment(createdAt).format('YYYY-MM-DD')}
+                </div>
+                <div
+                  className='lost-list-content-area-state'
+                  style={{ color: LostStatusColor[lostStatus] }}
+                >
+                  {LostStatus[lostStatus]}
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
-              </div>
-            </div>
-          </div>
-
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
-              </div>
-            </div>
-          </div>
-
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
-              </div>
-            </div>
-          </div>
-
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
-              </div>
-            </div>
-          </div>
-
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
-              </div>
-            </div>
-          </div>
-
-          <div className='lost-list-content'>
-            <div className='lost-list-content-image'></div>
-            <div className='lost-list-content-title'>청소기주세요.</div>
-            <div className='lost-list-content-place'>503호</div>
-            <div className='lost-list-content-area'>
-              <div className='lost-list-content-area-date'>2020-11-23</div>
-              <div className='lost-list-content-area-state' style={{ color: '#C41E1E' }}>
-                미완료
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
   );
 };
 
-Lost.propTypes = {};
+Lost.propTypes = {
+  losts: PropTypes.array,
+  getLosts: PropTypes.func,
+  clearLostStore: PropTypes.func,
+};
 
 export default Lost;

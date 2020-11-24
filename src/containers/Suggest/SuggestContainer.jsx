144419780a81a963907d4bdc1baaf68c00a41814
_uchanlee <uchanlee.dev@gmail.com>
2020-11-24 23:15:43 +0900

    ADD: connect Mobx and API

--- src/containers/Suggest/SuggestContainer.jsx
+++ src/containers/Suggest/SuggestContainer.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { observer } from 'mobx-react';
 import Suggest from '../../components/Suggest/Suggest';
+import { toast } from 'react-toastify';
+import useStore from 'lib/hooks/useStore';
 
 const SuggestContainer = ({}) => {
+  const { store } = useStore();
+  const { articles, getArticlesStatusCode, getArticles, clearArticleStor } = store.ArticleStore;
+
+  const loadPage = useCallback(() => {
+    getArticles();
+  }, []);
+
+  useEffect(() => {
+    switch (Math.floor(getArticlesStatusCode / 100)) {
+      case 2:
+        toast.success('건의사항을 불러오는데 성공하였습니다.');
+        break;
+      case 4:
+        toast.error(`Code: ${getArticlesStatusCode} 불러오기 실패.`, {});
+    }
+  }, [getArticlesStatusCode]);
+
   return (
     <>
-      <Suggest />
+      <Suggest articles={articles} loadPage={loadPage} clearArticleStor={clearArticleStor} />
     </>
   );
 };

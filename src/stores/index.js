ac39c3ad9e1f014967e54bf6b46eb11ec08f093b
_uchanlee <uchanlee.dev@gmail.com>
2020-11-25 01:42:51 +0900

    ADD: LostStore

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,11 +2,13 @@ import AuthStore from './Auth';
 import ArticleStore from './ArticleStore';
 import AttendStore from './Attend';
 import NoticeStore from './Notice';
+import LostStroe from './Lost';
 
 const stores = {
   AuthStore: new AuthStore(),
   AttendStore: new AttendStore(),
   NoticeStore: new NoticeStore(),
   ArticleStore: new ArticleStore(),
+  LostStroe: new LostStroe(),
 };
 export default stores;

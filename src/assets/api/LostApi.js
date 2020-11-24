fd743468cb53b7525351e54168f19e8c6ef92dc6
_uchanlee <uchanlee.dev@gmail.com>
2020-11-25 05:44:41 +0900

    ADD: modify api

--- src/assets/api/LostApi.js
+++ src/assets/api/LostApi.js
@@ -5,8 +5,13 @@ class LostApi extends Client {
     super(Client.createClientWithAccessToken('lost-product'));
   }
 
+  init() {
+    this.setClient(Client.createClientWithAccessToken('lost-product'));
+  }
+
   async getArticles() {
     try {
+      this.init();
       return await this.getClient().get('');
     } catch (error) {
       throw error;
@@ -15,7 +20,17 @@ class LostApi extends Client {
 
   async searchQuery(query) {
     try {
+      this.init();
       return await this.getClient().get(`/search?title=${query}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async modifyLostStatus(lostProduct) {
+    try {
+      this.init();
+      return await this.getClient().patch('', lostProduct);
     } catch (error) {
       throw error;
     }

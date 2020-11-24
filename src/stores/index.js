fa665a249e93bf9551e40dbf6d06b2ede33b5e79
_uchanlee <uchanlee.dev@gmail.com>
2020-11-24 23:13:53 +0900

    ADD: ArticleStore

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,8 @@
 import AuthStore from './Auth';
+import ArticleStore from './ArticleStore';
 
 const stores = {
   AuthStore: new AuthStore(),
+  ArticleStore: new ArticleStore(),
 };
 export default stores;

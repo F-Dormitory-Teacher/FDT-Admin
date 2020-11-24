bcfce8ac3f0c0afbbefddf8a2a937dae007864a0
Jominhui <rihito2021@naver.com>
2020-11-24 13:47:32 +0900

    ADD: Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import NotFoundPage from "../pages/NotFoundPage";
 import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
+import NoticePage from "../pages/NoticePage";
+import SuggestPage from "../pages/SuggestPage";
+import LostPage from "../pages/LostPage";
+
 
 const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route component={NotFoundPage} />
-        <Redirect to="/notfound" />
+        <Route exact path="/" component={MainPage} />        
+        <Route path="/login" component={LoginPage} />
+        <Route path="/notice" component={NoticePage} />
+        <Route path="/suggest" component={SuggestPage} />
+        <Route path="/lost" component={LostPage} />
       </Switch>
     </div>
   );

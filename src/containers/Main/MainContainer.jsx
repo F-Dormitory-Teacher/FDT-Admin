a0375b9ceec6d470fdc6e8687677b9767295ff9c
_uchanlee <uchanlee.dev@gmail.com>
2020-11-25 06:23:40 +0900

    ADD: Connect Schedul mobx

--- src/containers/Main/MainContainer.jsx
+++ src/containers/Main/MainContainer.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { observer } from 'mobx-react';
 import useStore from '../../lib/hooks/useStore';
 import Attendance from '../../components/Attendance/Attendance';
 import { useHistory, withRouter } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const MainContainer = ({}) => {
   const [date, setDate] = useState('');
@@ -10,6 +11,17 @@ const MainContainer = ({}) => {
 
   const { store } = useStore();
   const { handleAttendInfo, attend } = store.AttendStore;
+
+  const {
+    isRunning,
+    getStatus,
+    turnOn,
+    turnOff,
+    clearLostStore,
+    isRunningStatusCode,
+    turnOnStatusCode,
+    turnOffStatusCode,
+  } = store.ScheduleStore;
 
   const history = useHistory();
 
@@ -19,9 +31,69 @@ const MainContainer = ({}) => {
     });
   };
 
+  const getStatusDispatch = useCallback(() => {
+    getStatus();
+  }, [getStatus]);
+
+  const turnOnDispatch = useCallback(() => {
+    turnOn();
+  }, [turnOn]);
+
+  const turnOffDispatch = useCallback(() => {
+    turnOff();
+  }, [turnOff]);
+
+  const clearLostStoreDispatch = useCallback(() => {
+    clearLostStore();
+  }, [clearLostStore]);
+
+  useEffect(() => {
+    switch (Math.floor(isRunningStatusCode / 100)) {
+      case 2:
+        toast.success('스케쥴러 상태를 불러오는데 성공하였습니다.');
+        break;
+      case 4:
+        toast.error(`Code: ${isRunningStatusCode} 불러오기 실패.`, {});
+    }
+  }, [isRunningStatusCode]);
+
+  useEffect(() => {
+    switch (Math.floor(turnOnStatusCode / 100)) {
+      case 2:
+        toast.success('스케쥴러 실행에 성공하였습니다.');
+        clearLostStoreDispatch();
+        getStatusDispatch();
+        break;
+      case 4:
+        toast.error(`Code: ${turnOnStatusCode} 불러오기 실패.`, {});
+    }
+  }, [turnOnStatusCode]);
+
+  useEffect(() => {
+    switch (Math.floor(turnOffStatusCode / 100)) {
+      case 2:
+        toast.success('스케줄러 정지에 성공하였습니다.');
+        clearLostStoreDispatch();
+        getStatusDispatch();
+        break;
+      case 4:
+        toast.error(`Code: ${turnOffStatusCode} 불러오기 실패.`, {});
+    }
+  }, [turnOffStatusCode]);
+
   return (
     <>
-      <Attendance attend={attend} setDate={setDate} setType={setType} listLoading={listLoading} />
+      <Attendance
+        attend={attend}
+        setDate={setDate}
+        setType={setType}
+        listLoading={listLoading}
+        isRunning={isRunning}
+        getStatus={getStatusDispatch}
+        turnOn={turnOnDispatch}
+        turnOff={turnOffDispatch}
+        clearLostStore={clearLostStoreDispatch}
+      />
     </>
   );
 };
